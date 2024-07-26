6057c3f2550b8aa105e714eb5c4a6bed93768e60
Engineer9736 <richard@rscheib.eu>
2024-07-26 09:41:41 +0200

    Created an 2D array of Tiles inside Board

--- board.cpp
+++ board.cpp
@@ -1,5 +1,16 @@
-class BoardC {
+#include <iostream>
+#include <list>
+#include "tile.cpp"
 
+using namespace std;
+
+
+
+
+class BoardC {
+    private:
+        Tile tiles[20][10]; // Board is 20 tiles wide and 10 tiles high.
+    
     
 
 
